0d8a067074fe078a8037e1bad5b2208ffd0afc71
Danny Textores <textores.danny@gmail.com>
2020-07-03 09:41:12 +0200

    add configuration in main.go

--- main.go
+++ main.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"example.com/service/api"
+	"fmt"
 	"github.com/apex/gateway"
 	"net/http"
 )
 
+type config struct {
+	environment string
+	local       bool
+}
+
+func (c config) ItemTableName() string {
+	return fmt.Sprintf("items-%s", c.environment)
+}
+
 func main() {
-	a := api.NewAPI()
+	config := config{
+		environment: "dev",
+		local:       true,
+	}
+
+	a := api.NewAPI(config)
 	var err error
-	if true {
+	if config.local {
 		// development configuration
 		err = http.ListenAndServe("0.0.0.0:8080", a.Router())
 	} else {
