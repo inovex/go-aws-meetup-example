636f19b1a4015223221e8e5dec0ec46888b19ce9
Danny Textores <textores.danny@gmail.com>
2020-07-03 09:34:45 +0200

    add feature configuration to feature router

--- feature/router.go
+++ feature/router.go
@@ -2,8 +2,24 @@ package feature
 
 import "github.com/go-chi/chi"
 
-func RouteExample(r chi.Router) {
-	// TODO
-	// r.Get("/items", CreateGetItemsHandler())
-	// r.Post("/items", CreateNewItemHandler())
+type Configuration interface {
+	ItemTableName() string
+}
+
+type Feature struct {
+	service *itemService
+	repo    *dynamoRepo
+}
+
+func Configure(cfg Configuration) Feature {
+	repo := newDynamoRepo(cfg.ItemTableName())
+	return Feature{
+		service: newItemService(repo),
+		repo:    repo,
+	}
+}
+
+func (feat Feature) RouteExample(r chi.Router) {
+	r.Get("/items", CreateGetItemsHandler(feat.repo))
+	r.Post("/items", CreateNewItemHandler(feat.service))
 }
