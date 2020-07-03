525668ffb46ebb87724b8d15494878c909605fa6
Danny Textores <textores.danny@gmail.com>
2020-07-03 08:56:01 +0200

    move api into its own package

--- api/api.go
+++ api/api.go
@@ -1,4 +1,4 @@
-package main
+package api
 
 import (
 	"example.com/service/feature"
--- main.go
+++ main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
+	"example.com/service/api"
 	"github.com/apex/gateway"
 	"net/http"
 )
 
 func main() {
-	api := NewAPI()
+	a := api.NewAPI()
 	var err error
 	if true {
 		// development configuration
-		err = http.ListenAndServe("0.0.0.0:8080", api.Router())
+		err = http.ListenAndServe("0.0.0.0:8080", a.Router())
 	} else {
 		// AWS configuration
-		err = gateway.ListenAndServe("0.0.0.0:80", api.Router())
+		err = gateway.ListenAndServe("0.0.0.0:80", a.Router())
 	}
 	if err != nil {
 		// TODO structured logging
