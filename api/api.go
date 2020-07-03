56999c565167ce5e68373df55cfabfa44097d0d0
Danny Textores <textores.danny@gmail.com>
2020-07-03 09:35:11 +0200

    implement feature in API

--- api/api.go
+++ api/api.go
@@ -3,14 +3,24 @@ package api
 import (
 	"example.com/service/feature"
 	"github.com/go-chi/chi"
+	"net/http"
 )
 
+// the API struct holds references to all the features of the service
 type API struct {
+	feat feature.Feature
 }
 
-func NewAPI() *API {
+// Configuration collects all the configurations of the features using interface composition
+type Configuration interface {
+	// at the moment, 'feature' is the only implemented feature
+	feature.Configuration
+}
+
+// NewAPI creates a new API with all the features initialized based on the given Configuration
+func NewAPI(cfg Configuration) *API {
 	api := new(API)
-	// TODO initialize API
+	api.feat = feature.Configure(cfg)
 	return api
 }
 
@@ -24,8 +34,8 @@ func (api API) Router() *chi.Mux {
 	})
 
 	// define all features here
-	// TODO change RouteExample into a higher order function, accepting all dependencies of the feature package
-	r.Route("/feature", feature.RouteExample)
+	// example feature:
+	r.Route("/feature", api.feat.RouteExample)
 
 	return r
 }
