fde3c63937c0c647728b87067db77d4988a56420
Danny Textores <textores.danny@gmail.com>
2020-07-03 09:15:31 +0200

    add simple example route

--- api/api.go
+++ api/api.go
@@ -17,6 +17,12 @@ func NewAPI() *API {
 func (api API) Router() *chi.Mux {
 	r := chi.NewRouter()
 
+	// simple example route:
+	r.Get("/hello", func(writer http.ResponseWriter, request *http.Request) {
+		writer.WriteHeader(200)
+		_, _ = writer.Write([]byte("Hello!"))
+	})
+
 	// define all features here
 	// TODO change RouteExample into a higher order function, accepting all dependencies of the feature package
 	r.Route("/feature", feature.RouteExample)
