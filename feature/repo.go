fe20782339ac20c821af4084edf791e0c7c3c10e
Danny Textores <textores.danny@gmail.com>
2020-07-03 09:33:33 +0200

    implement repository

--- feature/repo.go
+++ feature/repo.go
@@ -1,1 +1,37 @@
 package feature
+
+import (
+	"example.com/service/models"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+	"github.com/guregu/dynamo"
+)
+
+type dynaMock struct {
+	dynamodbiface.DynamoDBAPI
+}
+
+type dynamoRepo struct {
+	itemTable dynamo.Table
+	mock      dynaMock
+}
+
+func newDynamoRepo(itemTableName string) *dynamoRepo {
+	mock := dynaMock{}
+	db := dynamo.NewFromIface(mock)
+	return &dynamoRepo{
+		itemTable: db.Table(itemTableName),
+		mock:      dynaMock{},
+	}
+}
+
+func (d dynamoRepo) getItemByName(id string) (models.Item, error) {
+	panic("implement me")
+}
+
+func (d dynamoRepo) putItem(item models.Item) error {
+	panic("implement me")
+}
+
+func (d dynamoRepo) getItems() ([]models.Item, error) {
+	panic("implement me")
+}
--- go.mod
+++ go.mod
@@ -5,7 +5,8 @@ go 1.14
 require (
 	github.com/apex/gateway v1.1.1
 	github.com/aws/aws-lambda-go v1.17.0 // indirect
+	github.com/aws/aws-sdk-go v1.30.24
 	github.com/go-chi/chi v4.1.2+incompatible
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/guregu/dynamo v1.8.0
 	github.com/tj/assert v0.0.3 // indirect
 )
